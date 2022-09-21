9e7048bbe57cc6f40d49abe6e5033e7944ad682b
SamuelTasmaly <139364@student.vansd.org>
2022-09-21 10:40:28 -0700

    attached camera to vehicle

--- CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -2,20 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class FollowPlayer : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public GameObject player;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-    
+
     // Update is called once per frame
     void Update()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
-        
+        transform.position = player.transform.position + new Vector3(0,7,-12);
     }
 }
