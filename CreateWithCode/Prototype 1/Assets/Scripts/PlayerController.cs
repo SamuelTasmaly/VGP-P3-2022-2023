4561aed29d22e3ecd80827e127aae4743606ebab
SamuelTasmaly <139364@student.vansd.org>
2022-09-22 10:36:56 -0700

    LateUpdate

--- CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 5.0f;
+    public float turnSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,6 @@ public class PlayerController : MonoBehaviour
     {
         // Move the vehicle forward
         transform.Translate(Vector3.forward*Time.deltaTime*speed);
-        
+        transform.Translate(Vector3.right*Time.deltaTime*turnSpeed);
     }
 }
