5ced977d2593a42382686ff057f645e120fe8e6f
SamuelTasmaly <139364@student.vansd.org>
2022-10-10 10:18:13 -0700

    clean up code

--- CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,9 +18,13 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right*horizontalInput* Time.deltaTime*speed);
-        if(transform.position.x < -10) {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+
+        //keep player inbounds
+        if(transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
         }
-    
+        if(transform.position.x > xRange){
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
     }
 }
