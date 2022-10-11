c0837722cf68f8c8d8680ccb97a062b35c197ee1
SamuelTasmaly <139364@student.vansd.org>
2022-10-11 10:42:56 -0700

    fixed code and added projectile

--- CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Prototype2/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float horizontalInput;
     public float speed = 10.0f;
     public float xRange = 10;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,11 +21,16 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.right*horizontalInput* Time.deltaTime*speed);
 
         //keep player inbounds
-        if(transform.position.x < -xRange) {
-            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        if(transform.position.x < -10){
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
         }
-        if(transform.position.x > xRange){
-            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        if(transform.position.x > 10){
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)){
+            //Launch projectile from player
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
         }
     }
 }
