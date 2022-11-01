0431b2a7ab10e57989bb2439a1a69144376ab66b
SamuelTasmaly <139364@student.vansd.org>
2022-11-01 10:42:53 -0700

    Fixed bugs

--- CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ CreateWithCode/Prototype2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,25 +8,33 @@ public class SpawnManagerX : MonoBehaviour
 
     private float spawnLimitXLeft = -22;
     private float spawnLimitXRight = 7;
-    private float spawnPosY = 40;
+    private float spawnLimitYTop = 40;
+    private float spawnLimitYBottom = 30;
+    private float spawnPosY = 30;
 
-    private float startDelay = 1.0f;
-    private float spawnInterval = 4.0f;
+    private 
 
     // Start is called before the first frame update
     void Start()
     {
+        float startDelay = 1.0f;
+        float spawnInterval = 3.0f;
         InvokeRepeating("SpawnRandomBall", startDelay, spawnInterval);
+
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        
         // Generate random ball index and random spawn position
-        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+        Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), Random.Range(spawnLimitYBottom,spawnLimitYTop), 0);
+
+        int ballIndex = Random.Range(0, ballPrefabs.Length);
 
         // instantiate ball at random spawn location
-        Instantiate(ballPrefabs[0], spawnPos, ballPrefabs[0].transform.rotation);
+        Instantiate(ballPrefabs[ballIndex], spawnPos, ballPrefabs[ballIndex].transform.rotation);
+        
     }
 
 }
