b99bc2d559dea04e9696c1f4f84912b1dc41c535
SamuelTasmaly <139364@student.vansd.org>
2022-11-30 10:39:53 -0800

    added destroy out of bounds

--- CreateWithCode/Prototype 3/Assets/Scripts/MoveLeft.cs
+++ CreateWithCode/Prototype 3/Assets/Scripts/MoveLeft.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class MoveLeft : MonoBehaviour
 {
     private float speed = 14;
-
+    private float leftBound = -15;
     private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
@@ -14,15 +14,15 @@ public class MoveLeft : MonoBehaviour
       GameObject.Find("Player").GetComponent<PlayerController>();
 
     }
-    
+
     // Update is called once per frame
     void Update()
     {
         if (playerControllerScript.gameOver == false){
           transform.Translate(Vector3.left * Time.deltaTime * speed);
         }
-
-
-
+        if (transform.position.x < leftBound && gameObject.CompareTag("Obstacle")){
+          Destroy(gameObject);
+        }
     }
 }
